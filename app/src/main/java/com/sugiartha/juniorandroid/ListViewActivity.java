67a9096c2bd998e229da8ba7d9c764bdc0fe7447
Fathan <fathanshani.fs@gmail.com>
2023-06-27 08:16:39 +0700

    feature: change activity_list_view.xml, ListViewActivity.java

--- app/src/main/java/com/sugiartha/juniorandroid/ListViewActivity.java
+++ app/src/main/java/com/sugiartha/juniorandroid/ListViewActivity.java
@@ -3,18 +3,21 @@ package com.sugiartha.juniorandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 public class ListViewActivity extends AppCompatActivity {
-
-    ListView lvResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.list_view);
 
-        lvResult = (ListView) findViewById(R.id.lvResult);
+        ListView lvResult = (ListView) findViewById(R.id.lvResult);
+        WebView webView = (WebView) findViewById(R.id.web);
 
         String[][] dataNegara = new String[][]{{"Indonesia", "ASIA", "indonesia"},
                 {"Malaysia", "ASIA", "malaysia"}, {"Singapore", "ASIA", "singapore"},
@@ -25,5 +28,16 @@ public class ListViewActivity extends AppCompatActivity {
 
         NegaraAdapter adapter = new NegaraAdapter(ListViewActivity.this, dataNegara);
         lvResult.setAdapter(adapter);
+
+        lvResult.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                for (int i = 0; i < dataNegara.length; i++){
+                    if (i == position) {
+                        webView.loadUrl("https://google.com/search?q=negara+"+dataNegara[i][0]);
+                    }
+                }
+            }
+        });
     }
 }
